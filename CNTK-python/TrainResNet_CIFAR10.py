e3fa3938549b3ad10f4cf9c0c32f8bd3ec27deed
AsusLaptop <jiafeng5513@outlook.com>
2019-04-21 22:05:15 +0800

    fix cntk-python

--- CNTK-python/TrainResNet_CIFAR10.py
+++ CNTK-python/TrainResNet_CIFAR10.py
@@ -1,9 +1,3 @@
-# Copyright (c) Microsoft. All rights reserved.
-#
-# Licensed under the MIT license. See LICENSE.md file in the project root
-# for full license information.
-# ==============================================================================
-
 from __future__ import print_function
 import os
 import argparse
@@ -13,33 +7,35 @@ import numpy as np
 from cntk import cross_entropy_with_softmax, classification_error, reduce_mean
 from cntk import Trainer, cntk_py
 from cntk.io import MinibatchSource, ImageDeserializer, StreamDef, StreamDefs
-from cntk.learners import momentum_sgd, learning_parameter_schedule_per_sample, momentum_schedule
+from cntk.learners import momentum_sgd, learning_parameter_schedule, momentum_schedule
 from cntk.debugging import *
 from cntk.logging import *
 from resnet_models import *
 import cntk.io.transforms as xforms
 
 # Paths relative to current python file.
-abs_path   = os.path.dirname(os.path.abspath(__file__))
-data_path  = os.path.join(abs_path, "..",  "CIFAR-10")
+abs_path = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(abs_path, "..", "CIFAR-10")
 
 # model dimensions
 image_height = 32
-image_width  = 32
-num_channels = 3 # RGB
-num_classes  = 10
+image_width = 32
+num_channels = 3  # RGB
+num_classes = 10
+
 
 # Define the reader for both training and evaluation action.
 def create_image_mb_source(map_file, mean_file, train, total_number_of_samples):
     if not os.path.exists(map_file) or not os.path.exists(mean_file):
-        raise RuntimeError("File '%s' or '%s' does not exist. Please run install_cifar10.py from DataSets/CIFAR-10 to fetch them" %
-                           (map_file, mean_file))
+        raise RuntimeError(
+            "File '%s' or '%s' does not exist. Please run install_cifar10.py from DataSets/CIFAR-10 to fetch them" %
+            (map_file, mean_file))
 
     # transformation pipeline for the features has jitter/crop only when training
     transforms = []
     if train:
         transforms += [
-            xforms.crop(crop_type='randomside', side_ratio=(0.8, 1.0), jitter_type='uniratio') # train uses jitter
+            xforms.crop(crop_type='randomside', side_ratio=(0.8, 1.0), jitter_type='uniratio')  # train uses jitter
         ]
     transforms += [
         xforms.scale(width=image_width, height=image_height, channels=num_channels, interpolations='linear'),
@@ -47,17 +43,30 @@ def create_image_mb_source(map_file, mean_file, train, total_number_of_samples):
     ]
     # deserializer
     return MinibatchSource(ImageDeserializer(map_file, StreamDefs(
-        features=StreamDef(field='image', transforms=transforms), # first column in map file is referred to as 'image'
-        labels=StreamDef(field='label', shape=num_classes))),     # and second as 'label'
-        randomize=train,
-        max_samples=total_number_of_samples,
-        multithreaded_deserializer=True)
+        features=StreamDef(field='image', transforms=transforms),  # first column in map file is referred to as 'image'
+        labels=StreamDef(field='label', shape=num_classes))),  # and second as 'label'
+                           randomize=train,
+                           max_samples=total_number_of_samples,
+                           multithreaded_deserializer=True)
 
 
 # Train and evaluate the network.
-def train_and_evaluate(reader_train, reader_test, network_name, epoch_size, max_epochs, profiler_dir=None,
+def train_and_evaluate(reader_train, reader_test, network_name, epoch_size, max_epochs, minibatch_size,
                        model_dir=None, log_dir=None, tensorboard_logdir=None, gen_heartbeat=False, fp16=False):
+    """
 
+    :param reader_train:
+    :param reader_test:
+    :param network_name:
+    :param epoch_size:    一个epoch有多少样本
+    :param max_epochs:    训练多少个epoch
+    :param model_dir:
+    :param log_dir:
+    :param tensorboard_logdir:
+    :param gen_heartbeat:
+    :param fp16:
+    :return:
+    """
     set_computation_network_trace_level(0)
 
     # Input variables denoting the features and label data
@@ -76,10 +85,10 @@ def train_and_evaluate(reader_train, reader_test, network_name, epoch_size, max_
         # create model, and configure learning parameters
         if network_name == 'resnet20':
             z = create_cifar10_model(graph_input, 3, num_classes)
-            lr_per_mb = [1.0]*80 + [0.1]*40 + [0.01]
+            lr_per_mb = [1.0] * 80 + [0.1] * 40 + [0.01]
         elif network_name == 'resnet110':
             z = create_cifar10_model(graph_input, 18, num_classes)
-            lr_per_mb = [0.1]*1 + [1.0]*80 + [0.1]*40 + [0.01]
+            lr_per_mb = [0.1] * 1 + [1.0] * 80 + [0.1] * 40 + [0.01]
         else:
             raise RuntimeError("Unknown model name!")
 
@@ -92,16 +101,31 @@ def train_and_evaluate(reader_train, reader_test, network_name, epoch_size, max_
         pe = C.cast(pe, dtype=np.float32)
 
     # shared training parameters
-    minibatch_size = 128
     l2_reg_weight = 0.0001
 
     # Set learning parameters
-    lr_per_sample = [lr/minibatch_size for lr in lr_per_mb]
-    lr_schedule = learning_parameter_schedule_per_sample(lr_per_sample, epoch_size=epoch_size)
+    lr_per_sample = []
+    #check_point=[80,120,160,180]
+    check_point = [5, 10, 15, 20]
+    lrs=[1e-3,1e-4,1e-5,1e-6,6e-7]
+    for i in range(max_epochs+1):
+        if i in range(0,check_point[0]):
+            lr_per_sample.append(lrs[0])
+        if i in range(check_point[0], check_point[1]):
+            lr_per_sample.append(lrs[1])
+        if i in range(check_point[1], check_point[2]):
+            lr_per_sample.append(lrs[2])
+        if i in range(check_point[2], check_point[3]):
+            lr_per_sample.append(lrs[3])
+        if i>check_point[3]:
+            lr_per_sample.append(lrs[4])
+
+    lr_schedule = learning_parameter_schedule(lr_per_sample, minibatch_size=minibatch_size, epoch_size=epoch_size)
     mm_schedule = momentum_schedule(0.9, minibatch_size)
 
     # progress writers
-    progress_writers = [ProgressPrinter(tag='Training', log_to_file=log_dir, num_epochs=max_epochs, gen_heartbeat=gen_heartbeat)]
+    progress_writers = [
+        ProgressPrinter(tag='Training', log_to_file=log_dir, num_epochs=max_epochs, gen_heartbeat=gen_heartbeat)]
     tensorboard_writer = None
     if tensorboard_logdir is not None:
         tensorboard_writer = TensorBoardProgressWriter(freq=10, log_dir=tensorboard_logdir, model=z)
@@ -118,18 +142,16 @@ def train_and_evaluate(reader_train, reader_test, network_name, epoch_size, max_
         label_var: reader_train.streams.labels
     }
 
-    log_number_of_parameters(z) ; print()
-
-    # perform model training
-    if profiler_dir:
-        start_profiler(profiler_dir, True)
-
-    for epoch in range(max_epochs):       # loop over epochs
+    log_number_of_parameters(z);
+    print("*********Training Start*********")
+    start = time.clock()
+    for epoch in range(max_epochs):  # loop over epochs
         sample_count = 0
         while sample_count < epoch_size:  # loop over minibatches in the epoch
-            data = reader_train.next_minibatch(min(minibatch_size, epoch_size-sample_count), input_map=input_map) # fetch minibatch.
-            trainer.train_minibatch(data)                                   # update model with it
-            sample_count += trainer.previous_minibatch_sample_count         # count samples processed so far
+            data = reader_train.next_minibatch(min(minibatch_size, epoch_size - sample_count),
+                                               input_map=input_map)  # fetch minibatch.
+            trainer.train_minibatch(data)  # update model with it
+            sample_count += trainer.previous_minibatch_sample_count  # count samples processed so far
 
         trainer.summarize_training_progress()
 
@@ -140,10 +162,8 @@ def train_and_evaluate(reader_train, reader_test, network_name, epoch_size, max_
 
         if model_dir:
             z.save(os.path.join(model_dir, network_name + "_{}.dnn".format(epoch)))
-        enable_profiler() # begin to collect profiler data after first epoch
+        enable_profiler()  # begin to collect profiler data after first epoch
 
-    if profiler_dir:
-        stop_profiler()
 
     # Evaluation parameters
     test_epoch_size = 10000
@@ -167,35 +187,27 @@ def train_and_evaluate(reader_train, reader_test, network_name, epoch_size, max_
     print("")
     trainer.summarize_test_progress()
     print("")
+    elapsed = (time.clock() - start)
+    print("Time used (s):", elapsed)
+    return metric_numer / metric_denom
 
-    return metric_numer/metric_denom
 
-if __name__=='__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--network', help='network type, resnet20 or resnet110', required=False, default='resnet20')
-    parser.add_argument('-e', '--epochs', help='total epochs', type=int, required=False, default='160')
-    parser.add_argument('-es', '--epoch_size', help='Size of epoch in samples', type=int, required=False, default='50000')
-    parser.add_argument('-p', '--profiler_dir', help='directory for saving profiler output', required=False, default=None)
-    parser.add_argument('-tensorboard_logdir', '--tensorboard_logdir', help='Directory where TensorBoard logs should be created', required=False, default=None)
-    parser.add_argument('-datadir', '--datadir', help='Data directory where the CIFAR dataset is located', required=False, default=data_path)
-    parser.add_argument('-outputdir', '--outputdir', help='Output directory for checkpoints and models', required=False, default=None)
-    parser.add_argument('-logdir', '--logdir', help='Log file', required=False, default=None)
-    parser.add_argument('-genheartbeat', '--genheartbeat', help="Turn on heart-beat for philly", action='store_true', default=False)
-    parser.add_argument('-fp16', '--fp16', help="use float16", action='store_true', default=False)
+if __name__ == '__main__':
+    epochs = 200
+    epoch_size = 50000
+    network_name = "resnet20"
+    model_dir = os.path.join(abs_path, "Models")
+    log_dir = None # None for Console Out,File path for log file
+    tensorboard_logdir = "./tfboard_log/"
+    minibatch_size = 32
 
-    args = vars(parser.parse_args())
-    epochs = args['epochs']
-    epoch_size = args['epoch_size']
-    network_name = args['network']
+    reader_train = create_image_mb_source(os.path.join(data_path, 'train_map.txt'),
+                                          os.path.join(data_path, 'CIFAR-10_mean.xml'),
+                                          True, total_number_of_samples=epochs * epoch_size)
 
-    model_dir = args['outputdir']
-    if not model_dir:
-        model_dir = os.path.join(abs_path, "Models")
+    reader_test = create_image_mb_source(os.path.join(data_path, 'test_map.txt'),
+                                         os.path.join(data_path, 'CIFAR-10_mean.xml'),
+                                         False, total_number_of_samples=C.io.FULL_DATA_SWEEP)
 
-    data_path = args['datadir']
-
-    reader_train = create_image_mb_source(os.path.join(data_path, 'train_map.txt'), os.path.join(data_path, 'CIFAR-10_mean.xml'), True, total_number_of_samples=epochs * epoch_size)
-    reader_test = create_image_mb_source(os.path.join(data_path, 'test_map.txt'), os.path.join(data_path, 'CIFAR-10_mean.xml'), False, total_number_of_samples=C.io.FULL_DATA_SWEEP)
-
-    train_and_evaluate(reader_train, reader_test, network_name, epoch_size, epochs, args['profiler_dir'], model_dir,
-                       args['logdir'], args['tensorboard_logdir'], gen_heartbeat=args['genheartbeat'], fp16=args['fp16'])
+    train_and_evaluate(reader_train, reader_test, network_name, epoch_size, epochs, minibatch_size,
+                        model_dir, log_dir, tensorboard_logdir, False, False)
