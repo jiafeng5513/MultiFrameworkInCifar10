5ded3609c1cb593e3f7dbfd17676f0f0b81d5ea1
Anna-B234 <jiafeng5513@outloo.com>
2019-04-24 22:00:56 +0800

    add a tensorflow example

--- Tensorflow/resnet18.py
+++ Tensorflow/resnet18.py
@@ -303,8 +303,7 @@ class Resnet18:
                         examples_per_sec = log_frequency * batch_size / duration
                         sec_per_batch = float(duration / log_frequency)
 
-                        format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
-                                      'sec/batch)')
+                        format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f sec/batch)')
                         print(format_str % (datetime.now(), self._step, loss_value,
                                             examples_per_sec, sec_per_batch))
 
