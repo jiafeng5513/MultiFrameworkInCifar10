f67d46339d0f0483912bc06267ee7413469ccba5
Anna-B234 <jiafeng5513@outloo.com>
2019-04-29 11:43:42 +0800

    update tf,delete some useless files

---
+++ Tensorflow/Run.bat
@@ -0,0 +1,7 @@
+@python.exe ResNet20-cifar10-tf.py
+@python.exe ResNet20-cifar10-tf.py
+@python.exe ResNet20-cifar10-tf.py
+@python.exe ResNet20-cifar10-tf.py
+@python.exe ResNet20-cifar10-tf.py
+
+pause
